7189221f1d1ec34d41a840f47556610ee6422679
Thanush <thanushaaksh32@gmail.com>
2026-03-27 23:59:10 +0530

    REST routes for /users

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
 const PORT = 3000;
 
@@ -14,6 +15,10 @@ app.get('/hello', (req,res)=>{
 //url http://localhost:3000/hello
 //output {"name":"thanush","message":"Hello from your server","timestamp":"2026-03-26T17:37:47.460Z"}
 
+
+
+
+
 app.get('/greet/:name',(req,res) =>{
     const {name} =req.params;
     res.json({
@@ -23,17 +28,21 @@ app.get('/greet/:name',(req,res) =>{
 //url http://localhost:3000/greet/vimal
 //output {"message":"Hello vimal! welcome"}
 
+
+
+
 app.post('/echo',(req,res)=>{
     const body = req.body;
     res.json({
         received:body
     });
 });
-
-
 //URl ---> http://localhost:3000/echo
 //outreq {"name": "thanush", "age": 21 }
 //out res {"received": {"name": "thanush","age": 21}}
+
+
+
 
 app.get('/add/:a/:b',(req,res)=>{
     const a = Number(req.params.a);
@@ -42,9 +51,84 @@ app.get('/add/:a/:b',(req,res)=>{
         result:a+b
     });
 });
-
 //URl -- > http://localhost:3000/add/2/5
 //output {"result":7}
+
+
+
+
+
+let users = [
+   {id:1, name: 'cheesy' ,email:'cheesy@gmail.com'},
+    {id:2, name : 'jaeger', email:'jaeger@gamil.com'}
+];
+
+let nextId = 3;
+
+router.get('/',(req,res)=>{
+    res.json(users);
+});
+
+//url http://localhost:3000/users
+//Full users Fetch [{"id":1,"name":"cheesy","email":"cheesy@gmail.com"},{"id":2,"name":"jaeger","email":"jaeger@gamil.com"}]
+
+
+
+// === which basically check is value is same and its type too
+
+router.get('/:id',(req,res)=>{
+    const user = users.find(u => u.id === Number(req.params.id));
+    if(!user)return res.status(404).json({error:"Antha mari entha user um illa"});
+    res.json(user);
+});
+//url http://localhost:3000/users/2
+// {"id":2,"name":"jaeger","email":"jaeger@gamil.com"}  else {"error":"Antha mari entha user um illa"}
+
+
+router.post('/',(req,res)=> {
+    const {name,email} =req.body;
+    if(!name||!email){
+        return res.status(400).json({error:"Name and email is required"});
+    }
+ 
+    const newUser = {id:nextId++,name,email};
+    users.push(newUser);
+    res.status(201).json(newUser);
+    });
+    //url http://localhost:3000/users
+    // req {
+    // "name": "thanush",
+    // "email": "thanush@gmail.com"}
+
+
+router.patch('/:id',(req,res)=>{
+    const user = users.find(u=> u.id === Number(req.params.id));
+    if(!user) return res.status(404).json({error:"antha mari entha user um illa"});
+    Object.assign(user,req.body);
+    res.json(user);
+});
+
+// url http://localhost:3000/users/1
+// req {"name": "thanush black"}
+//res  {"id":1,"name":"thanush black","email":"cheesy@gmail.com"}
+
+
+router.delete('/:id',(req,res)=>{
+    const idx= users.findIndex(u => u.id === Number(req.params.id));
+    if(idx === -1) return res.status(404).json({error:"antha mari entha user um illa"});
+
+       const deletedUser = users.splice(idx, 1);
+    res.json({ message: "User deleted", user: deletedUser[0] });
+});
+
+//url http://localhost:3000/users/1
+//res {"message":"User deleted","user":{"id":1,"name":"thanush black","email":"cheesy@gmail.com"}}
+
+
+
+// module.exports = router;
+app.use('/users', router);
+
 
 app.listen(PORT,()=>{
 console.log(`Server is currently running on http://localhost:${PORT}`);});
